4a6a7c4693650fbaa3a49eeff940381897f49fd9
vida <m.vidamohammadi@gmail.com>
2021-12-14 13:46:39 +0330

    useState

--- src/components/ProductForm/ProductForm.js
+++ src/components/ProductForm/ProductForm.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ProductForm.css'
 import Button from '../UI/Button/Button'
 import Input from '../UI/Input/Input'
 import Card from '../UI/Card/Card'
 
 const ProductForm = React.memo((props) => {
+    const [title, setTitle] = useState('')
+    const [amount, setAmount] = useState('')
+
     const submitHandler = (event) => {
         event.preventDefault()
     }
@@ -15,11 +18,25 @@ const ProductForm = React.memo((props) => {
                 <form onSubmit={submitHandler}>
                     <div className='form-control'>
                         <label htmlFor='title'>Title</label>
-                        <Input name="input" id="title"/>
+                        <Input 
+                            name="input" 
+                            id="title" 
+                            value={title} 
+                            onChange={(event) => 
+                                setTitle(event.target.value)
+                            } 
+                        />
                     </div>
                     <div className='form-control'>
                         <label htmlFor='amount'>Amount</label>
-                        <Input name="input" id="amount"/>
+                        <Input 
+                            name="input" 
+                            id="amount" 
+                            value={amount} 
+                            onChange={(event) => 
+                                setAmount(event.target.value)
+                            } 
+                        />
                     </div>
                     <div className='product-form__actions'>
                         <Button type='submit' value="Submit"></Button>
